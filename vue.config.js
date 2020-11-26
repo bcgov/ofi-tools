dbaef3b9c0da49ca1a64abc770bda4b29ff7b75b
BrandonSharratt <brandon@highwaythreesolutions.com>
2020-11-26 09:06:27 -0800

    add image build

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
   // vue.config.js
+  let ofiroot = (process.env.VUE_APP_OFI_ROOT) ? process.env.VUE_APP_OFI_ROOT : "https://delivery.apps.gov.bc.ca/pub/dwds-ofi/";
   module.exports = {
-      pluginOptions: {
-          API_OFI_ROOT: "https://delivery.apps.gov.bc.ca/pub/dwds-ofi/",
-          CreateOrderFilteredSM: "order/createOrderFilteredSM/",
-          CreateOrderFiltered: "secure/order/createOrderFiltered/"
-      }
-  }
+    pluginOptions: {
+      API_OFI_ROOT: ofiroot,
+      CreateOrderFilteredSM: "order/createOrderFilteredSM/",
+      CreateOrderFiltered: "secure/order/createOrderFiltered/"
+     }
+   }
+  

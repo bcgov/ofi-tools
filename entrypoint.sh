85bfde1c31a210d5d33d7c83cc3c05372524e51c
BrandonSharratt <brandon@highwaythreesolutions.com>
2020-12-14 13:13:51 -0800

    tweaking where the sed happens

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 JSON_STRING=".VUE_APP_OFI_ROOT:\"${VUE_APP_OFI_ROOT}\";"
-rm /tmp/app*.js
 cp /app/js/app*.js /tmp/.
 sed -i "s@.VUE_APP_OFI_ROOT:\"https://delivery.apps.gov.bc.ca/pub/dwds-ofi/\";@${JSON_STRING}@" /tmp/app*.js
-yes | cp -rf /tmp/app*.js /app/js/
+rm -rf /app/js/app*.js
+cp -rf /tmp/app*.js /app/js/
 cd /etc
 caddy 

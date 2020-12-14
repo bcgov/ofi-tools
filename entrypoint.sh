025d09303ffa1b74adca232ca822abe0c1ea25fe
BrandonSharratt <brandon@highwaythreesolutions.com>
2020-12-14 13:56:21 -0800

    changed how filename is handled

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 JSON_STRING=".VUE_APP_OFI_ROOT:\"${VUE_APP_OFI_ROOT}\";"
-for file in $(ls /tmp/);
-  do export $FILENAME="${file##*/}";
+for file in $(ls /tmp/); do 
+  export $ORIGINAL_NAME="${file##*/}";
 done
-sed "s@.VUE_APP_OFI_ROOT:\"https://delivery.apps.gov.bc.ca/pub/dwds-ofi/\";@${JSON_STRING}@" /tmp/app*.js > /app/js/$FILENAME
+sed "s@.VUE_APP_OFI_ROOT:\"https://delivery.apps.gov.bc.ca/pub/dwds-ofi/\";@${JSON_STRING}@" /tmp/app*.js > /app/js/$ORIGINAL_NAME
 cd /etc
 caddy 
